bfb4771451413d0430d3e0bae050c04ac33bb071
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-05-20 22:08:35 -0400

    Add darwin SDLPerl check, etc.

--- bin/snake.pl
+++ bin/snake.pl
@@ -1,9 +1,19 @@
 #!perl
 use strict;
 use warnings;
+
+BEGIN {
+    if ( $^O eq 'darwin' && $^X ne 'SDLPerl' ) {
+        exec 'SDLPerl', $0, @ARGV or die "Failed to exec SDLPerl: $!";
+    }
+}
+
 use Games::Snake;
 
 # PODNAME: snake.pl
+# ABSTRACT: Play the game
 
 Games::Snake->new()->run();
+
+exit;
 
